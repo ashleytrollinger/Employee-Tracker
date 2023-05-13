c7ca05ee11bee6014ed6118ac2131da05c505a95
ashleytrollinger <ashley@trollingers.com>
2023-05-13 16:28:25 -0400

    switch case added to the inquirer response

--- db/server.js
+++ db/server.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -13,3 +13,31 @@ const db = mysql.createConnection(
     }
 );
 
+// Using Inquirer to prompt
+inquirer.prompt([
+    {
+        name: "action",
+        message: "What would you like to do today?",
+        type: "list",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update Employee Role"]
+    }
+])
+    .then(function (response) {
+        var selectedChoice = response.action;
+
+        switch (seclectedChoice) {
+            case "View All Departments":
+
+            case "View All Roles":
+
+            case "View All Employees":
+
+            case "Add A Department":
+
+            case "Add A Role":
+
+            case "Add An Employee":
+
+            case "Update Employee Role":
+        }
+    })
